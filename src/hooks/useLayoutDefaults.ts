25d50fe6d3dacf06b006e2615140b34d50af6f68
Lance Gliser <lance.gliser@howso.com>
2024-04-11 10:14:29 -0500

    Refactored away some duplicates in useLayoutDefaults

--- src/hooks/useLayoutDefaults.ts
+++ src/hooks/useLayoutDefaults.ts
@@ -1,17 +1,14 @@
-import { Layout } from "plotly.js";
+import type { Layout, LayoutAxis } from "plotly.js";
 import { useMemo } from "react";
 import { Gray } from "../colors";
 
-export function useLayoutDefaults({
-  colorScheme,
-}: UseLayoutDefaultsParams): Partial<Layout> {
-  const layoutDefaults = useMemo(
-    () => getLayoutDefaults({ colorScheme }),
-    [colorScheme]
-  );
+export const useLayoutDefaults = (
+  params: UseLayoutDefaultsParams
+): Partial<Layout> => {
+  const layoutDefaults = useMemo(() => getLayoutDefaults(params), [params]);
 
   return layoutDefaults;
-}
+};
 
 export type UseLayoutDefaultsParams = {
   colorScheme: "light" | "dark";
@@ -19,24 +16,34 @@ export type UseLayoutDefaultsParams = {
 /** A direct utility for getting defaults. For ease, you may prefer useLayoutDefaults. */
 export const getLayoutDefaults = ({
   colorScheme,
-}: UseLayoutDefaultsParams): Partial<Layout> => ({
-  autosize: true,
-  paper_bgcolor: "transparent",
-  plot_bgcolor: "transparent",
-  margin: {
-    // @ts-expect-error https://plotly.com/javascript/reference/layout/#layout-margin-autoexpand
-    autoexpand: true,
-  },
-  font: {
-    family: "Inter, system-ui, sans-serif",
-    color: colorScheme === "dark" ? "#fff" : "#000",
-  },
-  xaxis: {
+}: UseLayoutDefaultsParams): Partial<Layout> => {
+  const axisDefaults: Partial<LayoutAxis> = {
+    automargin: true,
+    ticks: "outside",
     gridcolor:
       colorScheme === "dark" ? Gray["dark"]["100"] : Gray["light"]["900"],
-  },
-  yaxis: {
-    gridcolor:
-      colorScheme === "dark" ? Gray["dark"]["100"] : Gray["light"]["900"],
-  },
-});
+    tickcolor:
+      colorScheme === "dark" ? Gray["dark"]["700"] : Gray["light"]["700"],
+  };
+
+  return {
+    autosize: true,
+    paper_bgcolor: "transparent",
+    plot_bgcolor: "transparent",
+    margin: {
+      // @ts-expect-error https://plotly.com/javascript/reference/layout/#layout-margin-autoexpand
+      autoexpand: true,
+    },
+    font: {
+      family: "Inter, system-ui, sans-serif",
+      color: colorScheme === "dark" ? "#fff" : "#000",
+    },
+    xaxis: {
+      ...axisDefaults,
+      tickangle: 90,
+    },
+    yaxis: {
+      ...axisDefaults,
+    },
+  };
+};
