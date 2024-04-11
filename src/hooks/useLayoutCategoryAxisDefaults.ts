a7396313e08efda9899e04492706e3c73bdfeacb
Lance Gliser <lance.gliser@howso.com>
2024-04-11 13:01:20 -0500

    Fixed a bug

--- src/hooks/useLayoutCategoryAxisDefaults.ts
+++ src/hooks/useLayoutCategoryAxisDefaults.ts
@@ -10,7 +10,7 @@ export const useLayoutCategoryAxisDefaults = (
   params: UseLayoutCategoryAxisDefaultsParams
 ): Partial<LayoutAxis> => {
   const layoutDefaults = useMemo(
-    () => useLayoutCategoryAxisDefaults(params),
+    () => getLayoutCategoryAxisDefaults(params),
     [params]
   );
 
