278edaf7590ed5bd3948f3ac02f98a61cf50b10f
Lance Gliser <lance.gliser@howso.com>
2024-04-12 09:58:11 -0500

    Added FormatCategoryTickTextParams

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,11 +1,37 @@
-export const formatCategoryTickText = (string: string): string => {
-  const limit = 15;
+export type FormatCategoryTickTextParams = {
+  /**
+   * A minimum size limit for chunks. Breaks will be produced after this size at the next word boundary.
+   * The first chunk and the last will be concatenated.
+   * Default: 15
+   **/
+  limit?: number;
+  /**
+   * If true, truncation will include `<br />` elements to force label line wraps between truncation chunks.
+   * Default: true
+   **/
+  wrap?: boolean;
+  /** A list of characters to replace with spaces as candidates for truncation breaks */
+  replacements?: string | RegExp;
+};
+export const formatCategoryTickText = (
+  string: string,
+  params: FormatCategoryTickTextParams = {
+    limit: 15,
+    wrap: true,
+  }
+): string => {
+  const limit = params.limit || 15;
+  const wrap = typeof params.wrap === "boolean" ? params.wrap : true;
 
   if (string.length < limit) {
     return string;
   }
+  const replacedString = params.replacements
+    ? string.replaceAll(params.replacements, " ")
+    : string;
+
   const regex = new RegExp(`.{1,${limit}}(?:\\s|$)`, "g");
-  const chunks = string.replaceAll("|", " ").match(regex);
+  const chunks = replacedString.match(regex);
   if (!chunks) {
     return "";
   }
@@ -13,5 +39,5 @@ export const formatCategoryTickText = (string: string): string => {
   return [chunks.at(0), chunks.at(-1)]
     .map((chunk) => (chunk || "").trim())
     .filter(Boolean)
-    .join("...<br />");
+    .join(wrap ? "...<br />" : " ... ");
 };
