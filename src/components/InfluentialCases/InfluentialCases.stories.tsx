3f553bf01bddb8071272c7f1c0db89991a768646
Lance Gliser <lance.gliser@howso.com>
2024-04-16 13:46:14 -0500

    Added InfluentialCases

---
+++ src/components/InfluentialCases/InfluentialCases.stories.tsx
@@ -0,0 +1,100 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { InfluentialCases } from "./InfluentialCases";
+import { isDarkBackground } from "../../../.storybook/utils";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof InfluentialCases> = {
+  component: InfluentialCases,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
+  tags: ["autodocs"],
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+  args: {
+    feature: "chol",
+    actualValue: 252,
+    influenceCases: [
+      {
+        ".session": "3028ea6e-de18-41cf-8579-07eb0304059d",
+        chol: 264,
+        thal: 2,
+        exang: 0,
+        ".session_training_index": 298,
+        age: 45,
+        sex: 1,
+        cp: 1,
+        thalach: 132,
+        ".influence_weight": 0.5645133348066479,
+        fbs: 0,
+        ca: 0,
+        slope: 2,
+        ecg: 0,
+        trestbps: 110,
+        class: 1,
+        oldpeak: 1.2,
+      },
+      {
+        ".session": "3028ea6e-de18-41cf-8579-07eb0304059d",
+        chol: 219,
+        thal: 2,
+        exang: 0,
+        ".session_training_index": 109,
+        age: 39,
+        sex: 1,
+        cp: 4,
+        thalach: 140,
+        ".influence_weight": 0.43548661040784065,
+        fbs: 0,
+        ca: 0,
+        slope: 2,
+        ecg: 0,
+        trestbps: 118,
+        class: 1,
+        oldpeak: 1.2,
+      },
+    ],
+    predictionCase: {
+      age: 50,
+      sex: 1,
+      cp: 1,
+      trestbps: 130,
+      chol: 262,
+      fbs: 0,
+      ecg: 0,
+      thalach: 140,
+      exang: 0,
+      oldpeak: 1.5,
+      slope: 2,
+      ca: 0,
+      thal: 2,
+      class: 1,
+    },
+    predictedValue: 262,
+    residual: 38.02014353196766,
+    stats: {
+      contribution: 0.04617695577760673,
+      mae: 52.80061553591218,
+      mda: 0.006652253390817597,
+    },
+    style: {
+      height: "90vh",
+      width: "90vw",
+      // overflow: "hidden",
+    },
+  },
+  render: (args, context) => {
+    return <InfluentialCases {...args} isDark={isDarkBackground(context)} />;
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof InfluentialCases>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const Default: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  args: {},
+};
---
+++ src/components/InfluentialCases/InfluentialCases.tsx
@@ -0,0 +1,367 @@
+import { CSSProperties, ReactNode, useMemo } from "react";
+import { getColorScheme, Blue, Purple, Orange, Green } from "../../colors";
+import { useLayoutDefaults } from "../../hooks";
+import { BaseVisualProps, plotDefaults } from "../BaseVisual";
+import { Layout, Data, ScatterMarkerLine } from "plotly.js";
+import Plot from "react-plotly.js";
+import type { PredictionStats } from "@howso/openapi-client";
+import {
+  isNA,
+  parseNA,
+  safeMax,
+  safeMin,
+  KERNELS,
+  kernelDensityEstimator,
+} from "../..";
+import { extent, range } from "d3-array";
+
+export type InfluentialCasesProps = BaseVisualProps & {
+  /** The actual value from react */
+  actualValue?: number;
+  className?: string;
+  feature: string;
+  //   formatParams?: Omit<FormatCategoryTickTextParams, "wrap">;
+  /**
+   * response.content?.boundary_cases?.[0] ?? [];
+   */
+  influenceCases: Record<string, string | number | null>[];
+  predictionCase: Record<string, string | number | null>;
+  predictedValue: number;
+  stats: PredictionStats | undefined | null;
+  /** react['details']['feature_residuals'][0][action_feature[0]] */
+  residual?: number;
+  /**
+   * An optional set of redefined screen sizes to use in breakpoint logic.
+   * Labels will be wrapped to a secondary line based on screen size.
+   *   sm: <= 10
+   *   md: <= 20
+   *   lg: <= 25
+   **/
+  //   screenSizes?: ScreenSizes;
+  style?: CSSProperties;
+};
+
+/**
+ * Displays a prediction with additional information for interpreting the result.
+ *
+ * @see https://www.figma.com/file/uipiKBGe2ma0EGfkioXdF2/Howso-Visuals?type=design&node-id=54-1354
+ * @see https://plotly.com/javascript/line-and-scatter/
+ * @see https://plotly.com/javascript/filled-area-plots/
+ * @see https://plotly.com/javascript/reference/scatter/
+ * @see https://plotly.com/javascript/multiple-axes/
+ */
+export function InfluentialCases({
+  actualValue,
+  feature,
+  //   formatParams,
+  influenceCases,
+  isDark,
+  isPrint,
+  layout: layoutProp,
+  name = "Influential cases",
+  predictionCase,
+  predictedValue,
+  residual,
+  //   screenSizes,
+  stats,
+  ...props
+}: InfluentialCasesProps): ReactNode {
+  const colorScheme = getColorScheme({ isDark, isPrint });
+
+  // Create layout
+  const layoutDefaults = useLayoutDefaults({ colorScheme });
+
+  const layout = useMemo((): Partial<Layout> => {
+    return {
+      ...layoutDefaults,
+      ...layoutProp,
+      xaxis: {
+        ...layoutDefaults.xaxis,
+        ...layoutProp?.xaxis,
+        title: {
+          text: feature,
+        },
+      },
+      yaxis: {
+        ...layoutDefaults.yaxis,
+        ...layoutProp?.yaxis,
+        title: "Density",
+        hoverformat: ".4f",
+      },
+      yaxis2: {
+        ...layoutDefaults.yaxis,
+        ...layoutProp?.yaxis,
+        title: "Influence weight (%)",
+        overlaying: "y",
+        side: "right",
+        zeroline: false,
+        gridcolor: "transparent",
+        hoverformat: ".4f",
+      },
+      //   annotations,
+      // legend: {},
+    };
+  }, [layoutDefaults, layoutProp, feature]);
+
+  // Create data
+  const data = useMemo((): Data[] => {
+    console.clear();
+    const data: Data[] = [];
+    const maxInfluenceWeight = getMaximumInfluenceWeight({ influenceCases });
+    const testValue = predictionCase[feature] as number;
+    const isTestValueNaN = isNA(testValue);
+    const isInteger = false; // TODO maybe?
+    const rawMae = parseNA(stats?.mae);
+    const mae = isInteger ? Math.ceil(rawMae) : rawMae;
+    const markerLine: Partial<ScatterMarkerLine> = {
+      width: 2,
+      color: colorScheme === "dark" ? "#fff" : "#000",
+    };
+
+    const xStats = getXValues({
+      feature,
+      influenceCases,
+      isTestValueNaN,
+      mae,
+      testValue,
+    });
+
+    if (predictedValue) {
+      const predictedValueData: Data = {
+        name: "Predicted value",
+        x: [predictedValue],
+        y: [maxInfluenceWeight * valuesYOffset],
+        yaxis: "y2",
+        type: "scatter",
+        mode: "markers",
+        marker: {
+          size: 15,
+          color: Purple[colorScheme]["900"],
+          line: markerLine,
+        },
+        hovertemplate: "Predicted: %{x}<br />Influence: N/A<extra></extra>",
+        error_x: !residual ? undefined : { type: "constant", value: residual },
+      };
+      console.info(predictedValueData);
+      data.push(predictedValueData);
+    }
+
+    if (actualValue) {
+      const actualValueData: Data = {
+        name: "Actual value",
+        x: [actualValue],
+        y: [maxInfluenceWeight * valuesYOffset],
+        yaxis: "y2",
+        type: "scatter",
+        mode: "markers",
+        marker: {
+          size: 15,
+          color: Orange[colorScheme]["900"],
+          line: markerLine,
+        },
+        hovertemplate: "Value: %{x}<br />Influence: N/A<extra></extra>",
+      };
+      console.info(actualValueData);
+      data.push(actualValueData);
+    }
+
+    // Influence cases
+    if (influenceCases) {
+      // return Math.max(
+      //   ...influenceCases.map((ic) => {
+      //     const value = ;
+      //     return typeof value === "number" ? parseNA(value) : 0;
+      //   })
+      // );
+      const influenceValues = influenceCases.reduce(
+        (values, ic) => {
+          const rawX = ic[feature];
+          const xValue = typeof rawX === "number" ? parseNA(rawX) : undefined;
+          const rawY = ic[".influence_weight"];
+          const yValue = typeof rawY === "number" ? parseNA(rawY) : undefined;
+          const indexRaw = ic[".session_training_index"];
+          const index = indexRaw ? indexRaw.toString() : "";
+
+          if (xValue === undefined || yValue === undefined) {
+            return values;
+          }
+
+          values.x.push(xValue);
+          values.y.push(yValue);
+          values.text.push(index);
+          return values;
+        },
+        { x: [], y: [], text: [] } as {
+          x: number[];
+          y: number[];
+          text: string[];
+        }
+      );
+
+      const influenceData: Data = {
+        name: "Influence case",
+        x: influenceValues.x,
+        y: influenceValues.y,
+        yaxis: "y2",
+        type: "scatter",
+        mode: "markers",
+        marker: {
+          size: 15,
+          color: Green[colorScheme]["900"],
+          line: markerLine,
+        },
+        hovertemplate:
+          "Value: %{x}<br />Influence: %{y}%<br />Index: %{text}<extra></extra>",
+        text: influenceValues.text,
+      };
+      console.info(influenceData);
+      data.push(influenceData);
+    }
+
+    // Density
+    if (xStats.xDelta) {
+      const increment = Math.ceil((xStats.totalMax - xStats.totalMin) / 10);
+      const ticks = range(xStats.totalMin, xStats.totalMax, increment);
+      if (ticks[ticks.length - 1] < xStats.totalMax)
+        ticks.push(xStats.totalMax); // ensure max is in ticks
+      const kde = kernelDensityEstimator(
+        KERNELS.epanechnikov(increment),
+        ticks
+      );
+      const density = kde(xStats.values).reduce(
+        (values, [x, y]) => {
+          values.x.push(x);
+          values.y.push(y); // TODO Round to 6 you say...?
+          return values;
+        },
+        { x: [], y: [] } as { x: number[]; y: number[] }
+      );
+
+      const densityData: Data = {
+        name: "Density",
+        x: density.x,
+        y: density.y,
+        type: "scatter",
+        mode: "lines",
+        fill: "tozeroy",
+        hoverinfo: "skip",
+        line: {
+          color: Blue[colorScheme]["900"],
+        },
+        opacity: 0.2,
+      };
+      console.info(densityData);
+      data.push(densityData);
+
+      //   const dataPoints: ApexDataPoint[] = [];
+      //   for (const row of cases) {
+      //     if (!isNA(row[selectedFeature])) {
+      //       dataPoints.push({
+      //         x: row[selectedFeature],
+      //         y: roundTo(row[".influence_weight"] * 100, 2),
+      //         meta: { orig: row },
+      //       });
+      //     }
+      //   }
+    }
+
+    return data;
+  }, [
+    actualValue,
+    colorScheme,
+    feature,
+    influenceCases,
+    predictionCase,
+    predictedValue,
+    residual,
+    stats,
+  ]);
+
+  return (
+    <Plot
+      {...plotDefaults}
+      {...props}
+      data={data}
+      layout={layout}
+      config={plotDefaults.config}
+    />
+  );
+}
+
+const valuesYOffset = 1.05;
+
+type MaximumInfluenceWeightParams = Pick<
+  InfluentialCasesProps,
+  "influenceCases"
+>;
+const getMaximumInfluenceWeight = ({
+  influenceCases,
+}: MaximumInfluenceWeightParams): number => {
+  if (!influenceCases) {
+    return 0;
+  }
+  return Math.max(
+    ...influenceCases.map((ic) => {
+      const value = ic[".influence_weight"];
+      return typeof value === "number" ? parseNA(value) : 0;
+    })
+  );
+};
+
+type XValuesParams = {
+  isTestValueNaN: boolean;
+  /** Mean absolute error */
+  mae: number;
+  testValue: number;
+} & Pick<InfluentialCasesProps, "feature" | "influenceCases">;
+type XValues = {
+  xMin: number;
+  xMax: number;
+  xDelta: number;
+  totalMin: number;
+  totalMax: number;
+  /* The x value from all influence cases */
+  values: number[];
+};
+const getXValues = ({
+  feature,
+  influenceCases,
+  isTestValueNaN,
+  mae,
+  testValue,
+}: XValuesParams): XValues => {
+  const xValues = !influenceCases
+    ? []
+    : (influenceCases
+        .map((ic) => ic[feature])
+        .filter((ic) => !isNA(ic)) as number[]);
+  const [xMin = 0, xMax = 0] = extent(xValues);
+  const xDelta = safeMax(
+    1,
+    (safeMax(xMax, testValue + mae) - safeMin(xMin, testValue - mae)) / 10
+  );
+
+  if (isTestValueNaN) {
+    const totalMin = safeMax(0, xMin - xDelta);
+    const totalMax = xMax + xDelta;
+    return {
+      xMin,
+      xMax,
+      xDelta,
+      totalMin,
+      totalMax,
+      values: xValues,
+    };
+  }
+
+  const totalMin = safeMax(0, safeMin(testValue - mae - xDelta, xMin - xDelta));
+  const totalMax = safeMax(testValue + mae + xDelta, xMax + xDelta);
+  return {
+    xMin,
+    xMax,
+    xDelta,
+    totalMin,
+    totalMax,
+    values: xValues,
+  };
+};
