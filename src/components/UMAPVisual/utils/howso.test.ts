f42ec8ac3f45f80ed3bd3e25be4bc0199a6c41a2
Lance Gliser <lance.gliser@howso.com>
2024-07-12 10:38:35 -0500

    getUMAPKNNParamsFromHowsoTraineeDistances needs to run sorting

--- src/components/UMAPVisual/utils/howso.test.ts
+++ src/components/UMAPVisual/utils/howso.test.ts
@@ -1,19 +1,17 @@
-import { UMAP } from "umap-js";
 import { getUMAPKNNParamsFromHowsoTraineeDistances } from "./howso";
-import { irisData, irisDistances } from "@/data";
 
 describe("UMAPVisual/utils", () => {
   describe("getUMAPKNNParamsFromHowsoTraineeDistances", () => {
     it("should return parameters for knn initialization", () => {
       const distances = {
         "0": {
-          "0": Math.random() * 5,
-          "1": Math.random() * 2,
-          "2": Math.random() * 12,
+          "0": 0.8,
+          "1": 12,
+          "2": 5,
         },
         "1": {
           "0": Math.random() * 5,
-          "1": Math.random() * 2,
+          "1": 0.8,
           "2": Math.random() * 12,
         },
       };
@@ -21,26 +19,9 @@ describe("UMAPVisual/utils", () => {
       const { knnDistances, knnIndices } =
         getUMAPKNNParamsFromHowsoTraineeDistances(distances);
       expect(knnDistances.length).toBe(2);
-      expect(knnDistances[0].length).toBe(3);
       expect(knnIndices.length).toBe(2);
-      expect(knnIndices[0].length).toBe(3);
+      expect(knnDistances[0]).toStrictEqual([0.8, 5, 12]);
+      expect(knnIndices[0]).toStrictEqual([0, 2, 1]);
     });
-  });
-
-  describe("UMAP fit data", () => {
-    const { knnDistances, knnIndices } =
-      getUMAPKNNParamsFromHowsoTraineeDistances(irisDistances);
-
-    const umap = new UMAP({});
-
-    if (knnIndices?.length && knnDistances?.length) {
-      console.info("Setting knnIndices and knnDistances");
-      umap.setPrecomputedKNN(knnIndices, knnDistances);
-    }
-
-    // @ts-expect-error We're safe to ignore Vector shape as we called setPrecomputedKNN earlier
-    const positions = umap.fit(irisData);
-    console.info(positions);
-    expect(positions).toBeFalsy();
   });
 });
--- src/components/UMAPVisual/utils/howso.ts
+++ src/components/UMAPVisual/utils/howso.ts
@@ -1,5 +1,6 @@
 import { UMAPVisualKNNProps } from "../UMAPVisual";
 
+/** Returns the indices and distances from a Howso Trainee in asc order */
 export const getUMAPKNNParamsFromHowsoTraineeDistances = (
   distances: Record<string, Record<string, number>>
 ): {
@@ -10,12 +11,21 @@ export const getUMAPKNNParamsFromHowsoTraineeDistances = (
     throw new Error("distances is undefined");
   }
 
-  return {
-    knnIndices: Object.values(distances).map((vectorObject) =>
-      Object.keys(vectorObject).map(Number)
-    ),
-    knnDistances: Object.values(distances).map((vectorObject) =>
-      Object.values(vectorObject)
-    ),
-  };
+  const knnDistances: number[][] = [];
+  const knnIndices: number[][] = [];
+
+  Object.values(distances).forEach((distances, fromIndex) => {
+    knnDistances.push([]);
+    knnIndices.push([]);
+
+    Object.values(distances)
+      .map((distance, index) => ({ distance, index }))
+      .sort((a, b) => a.distance - b.distance)
+      .forEach(({ distance, index }) => {
+        knnDistances[fromIndex].push(distance);
+        knnIndices[fromIndex].push(index);
+      });
+  });
+
+  return { knnIndices, knnDistances };
 };
