519c81e0370fcf76838c8dd86ea4b570537ad4a8
Lance Gliser <lance.gliser@howso.com>
2025-04-16 14:21:36 -0500

    0000: Updating dependencies, Inter font, fixed a bug in discrete colorway that caused #526172 to repeat (#24)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,27 @@
 import type { Preview } from "@storybook/react";
-// CSS
-// import "@fontsource/inter";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/700.css";
 import { darkBackground, lightBackground } from "./constants";
+// CSS
+import "@fontsource-variable/inter";
+import "@fontsource-variable/inter/wght-italic.css";
 import "./preview.css";
 
 const preview: Preview = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    // layout: "fullscreen", // TODO not having any effect..
     backgrounds: {
       // Load them by hand, or make a nice loop if you wish
       values: [lightBackground, darkBackground],
       // Ensure a default is set, so you avoid type errors reading from undefined!
       default: lightBackground.name,
+      // Supports globals usage
+      options: {
+        [lightBackground.name]: lightBackground,
+        [darkBackground.name]: darkBackground,
+      },
     },
     controls: {
       matchers: {
+        color: /(background|color)$/i,
         date: /Date$/,
       },
     },
@@ -25,7 +29,6 @@ const preview: Preview = {
       viewport: { value: undefined },
     },
   },
-
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 };
 export default preview;
